a684582efc144734860945252cb7e8fccc1a6f6e
ruhiel <ruhiel1983@gmail.com>
2015-09-20 00:59:57 +0900

    初期コミット

---
+++ RepairTimePlugin/RepairTimePlugin.cs
@@ -0,0 +1,36 @@
+﻿using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+using Livet;
+using MetroTrilithon.Lifetime;
+using MetroTrilithon.Mvvm;
+using Nekoxy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairTimePlugin
+{
+    [Export(typeof(IPlugin))]
+    [ExportMetadata("Title", "入渠時間プラグイン")]
+    [ExportMetadata("Description", "入渠時間プラグイン")]
+    [ExportMetadata("Version", "1.0.0")]
+    [ExportMetadata("Author", "@ruhiel_murrue")]
+    [ExportMetadata("Guid", "8081BAEE-DD53-40CD-9C14-1FE2F098E58D")]
+    [Export(typeof(ITool))]
+    public class RepairTimePlugin : NotificationObject, IPlugin, ITool
+    {
+        public object View => new UserControl1 { DataContext = new RepairTimeViewModel(), };
+        public string Name => "RepairTimePlugin";
+
+        /*
+        */
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ RepairTimePlugin/RepairTimeViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using Livet;
+using Livet.EventListeners;
+using MetroTrilithon.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairTimePlugin
+{
+    public class RepairTimeViewModel : ViewModel
+    {
+        #region FirstFleet 変更通知プロパティ
+        private Ship[] _Ship;
+        public Ship[] FirstFleet
+        {
+            get { return _Ship;}
+            set
+            {
+                if (this._Ship != value)
+                {
+                    this._Ship = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+        #endregion
+        public RepairTimeViewModel()
+        {
+            KanColleClient.Current.Homeport.Organization
+                .Subscribe(nameof(Organization.Fleets), this.InitializeFleets)
+                .AddTo(this);
+
+            this.InitializeFleets();
+        }
+        public void InitializeFleets()
+        {
+            FirstFleet = KanColleClient.Current.Homeport.Organization.Fleets.Values.First().Ships;
+
+            this.CompositeDisposable.Add(new PropertyChangedEventListener(KanColleClient.Current.Homeport.Organization)
+            {
+                {
+                    () => KanColleClient.Current.Homeport.Organization.Ships,
+                    (_, __) => { DispatcherHelper.UIDispatcher.Invoke(this.UpdateView); }
+                }
+            });
+        }
+
+        private void UpdateView()
+        {
+            if (!KanColleClient.Current.IsStarted) return;
+
+            FirstFleet = KanColleClient.Current.Homeport.Organization.Fleets.Values.First().Ships;
+        }
+
+    }
+}
